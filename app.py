464ca2cdfeeaebe25404fe7cc6d8c89773ca48f5
Gandalem <rbals8429@gmail.com>
2025-12-12 09:28:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 # ⚙️ 설정 & Azure 연결
 # (배포했던 Azure 주소가 있다면 여기에 그대로 두세요)
 # -----------------------------------------------------------------------------
-AZURE_FUNCTION_URL = "https://stu456-game-api.azurewebsites.net/api/HttpTrigger1?code=euPgWVAwL_-v3RWH8iDu804DVzCAb-ptsOfeowcWTiHFAzFuQzSXOA==" 
+AZURE_FUNCTION_URL = "https://steam-api-c6evf9adg5gbcfbq.koreacentral-01.azurewebsites.net/api/HttpTrigger1" 
 
 st.set_page_config(layout="wide", page_title="게임 성공 예측 AI")
 
@@ -194,3 +194,4 @@ with st.expander("📚 모델 학습 및 성능 이력 (History)", expanded=True
         "상태": ["학습 완료", "학습 완료", "학습 완료", "배포 완료", "준비"]
     })
     st.dataframe(df_history, use_container_width=True, hide_index=True)
+
